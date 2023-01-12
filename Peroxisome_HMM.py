174e2ec10a195aa679ac5daa3e2f6e2b6bc83b22
cmsvensson <pmxcms1@gmail.com>
2023-01-12 08:30:47 +0100

    Started consolidating for github

--- Peroxisome_HMM.py
+++ Peroxisome_HMM.py
@@ -1,8 +1,13 @@
-#from tqdm import tqdm
-#import json
-#import os
-#import glob
-#import pandas as pd
+'''
+Copyright by Dr. Carl-Magnus Svensson
+Research Group Applied Systems Biology - Head: Prof. Dr. Marc Thilo
+Figge
+https://www.leibniz-hki.de/en/applied-systems-biology.html
+HKI-Center for Systems Biology of Infection
+Leibniz Institute for Natural Product Research and Infection Biology -
+Hans Knöll Insitute (HKI)
+Adolf-Reichwein-Straße 23, 07745 Jena, Germany
+'''
 import numpy as np
 from numba import jit
 
